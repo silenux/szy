d18d7d4dc053439bf87db687627958b6bf22e5a8
Teng <teng.zh@qq.com>
2016-03-03 16:24:35 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Szy::Application.config.secret_token = 'e9fda0b63b706e61306d12b3eeeb4df54ed8eebd060cfcd36c2fd206c35d57017069d65331eca66330957fec01ffcd0e34169a3b1b974a46271659c4c56b6cc1'
